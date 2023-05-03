acf6a134e805dafa7071f913ca65f1e6fe0ed6aa
Lorenzo Venerandi <68255980+Lore09@users.noreply.github.com>
2023-05-03 21:22:56 +0200

    Update main.cpp

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 // Motor A connections
 int enA = 6;
@@ -13,21 +13,21 @@ int in4 = 3;
 
 
 // Motor control variables
-int baseSpeed = 100;  // Base PWM value to sent to motors (Range 0-255), adjust to match your motors
-bool new_data = false; // Flag to indicate new data from serial communication
+int base_pwm = 100;  // Base PWM value to sent to motors (Range 0-255), adjust to match your motors
 
-int turnTarget = 0; // Target value for turning (Range -255 to 255)
-int currentTurn = 0; // Current turning value (Range -255 to 255)
+int turn_speed = 0; // Target value for turning (Range -255 to 255)
 
-int advanceTarget = 0; // Target value for advancing (Range -255 to 255)
-int currentAdvance = 0; // Current advancing value (Range -255 to 255)
+int advance_speed = 0; // Target value for advancing (Range -255 to 255)
 
 int leftMotorSpeed = 0;  // PWM value for left motor (Range 0 to 255)
 int rightMotorSpeed = 0; // PWM value for right motor (Range 0 to 255)
 
+long command_start_time = 0; // Time of last command
+long command_duration = 0; // Duration of last command
+
 // Servo control variables
-int servoTarget = 0; // Target value for servo (Range 0 to 180)
-int servoAngle = 0; // Current angle of servo (Range 0 to 180)
+//int servoTarget = 0; // Target value for servo (Range 0 to 180)
+//int servoAngle = 0; // Current angle of servo (Range 0 to 180)
 
 void stop(){
   digitalWrite(in1, LOW);
@@ -38,16 +38,12 @@ void stop(){
   analogWrite(enA, 0);
   analogWrite(enB, 0);
 
-  currentTurn = 0;
-  currentAdvance = 0;
-  advanceTarget = 0;
-  turnTarget = 0;
 }
 
 void moveMotors(int advanceSpeed, int turnSpeed){
 
-    leftMotorSpeed = advanceSpeed + turnSpeed;
-    rightMotorSpeed = advanceSpeed - turnSpeed;
+    rightMotorSpeed = advanceSpeed + turnSpeed;
+    leftMotorSpeed = advanceSpeed - turnSpeed;
 
     if(leftMotorSpeed == 0 && rightMotorSpeed == 0) {
       stop();
@@ -55,21 +51,21 @@ void moveMotors(int advanceSpeed, int turnSpeed){
     }
 
     if(leftMotorSpeed > 0) {
-        analogWrite(enA, leftMotorSpeed + baseSpeed);   
+        analogWrite(enA, leftMotorSpeed + base_pwm);   
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
     } else{
-        analogWrite(enA, -(leftMotorSpeed + baseSpeed));
+        analogWrite(enA, -(leftMotorSpeed - base_pwm));
         digitalWrite(in1, LOW);
         digitalWrite(in2, HIGH);
     }
 
     if(rightMotorSpeed > 0){
-        analogWrite(enB, rightMotorSpeed + baseSpeed);
+        analogWrite(enB, rightMotorSpeed + base_pwm);
         digitalWrite(in3, HIGH);
         digitalWrite(in4, LOW);
     } else {
-        analogWrite(enB, -(rightMotorSpeed + baseSpeed));
+        analogWrite(enB, -(rightMotorSpeed - base_pwm));
         digitalWrite(in3, LOW);
         digitalWrite(in4, HIGH);
     }
@@ -78,35 +74,76 @@ void moveMotors(int advanceSpeed, int turnSpeed){
 
 void turnLeft(){
   moveMotors(0, 50);
+  delay(400);
 }
 
 void turnRight(){
   moveMotors(0, -50);
+  delay(400);
 }
 
 void advance(){
   moveMotors(50, 0);
+  delay(800);
 }
 
 void back(){
   moveMotors(-50, 0);
+  delay(800);
 }
 
 
 bool checkSerialData(){
   // Read data from Serial Communication
   if (Serial.available() > 0) {
-    
+
     String data = Serial.readStringUntil('\n');
 
-    // split value,value,value by comma
-    int firstComma = data.indexOf(',');
-    int secondComma = data.indexOf(',', firstComma + 1);
+    // Direct commands
+    switch (data[0])
+    {
+    case 'A':
+      advance();
+      turn_speed=0;
+      advance_speed=0;
+      break;
+    
+    case 'B':
+      back();
+      turn_speed=0;
+      advance_speed=0;
+      break;
+    
+    case 'L':
+      turnLeft();
+      turn_speed=0;
+      advance_speed=0;
+      break;
+    
+    case 'R':
+      turnRight();
+      turn_speed=0;
+      advance_speed=0;
+      break;
+    
+    case 'S':
+      stop();
+      turn_speed=0;
+      advance_speed=0;
+      break;
+    
+    // Specific data
+    default:
+      // split value,value,value by comma
+      int firstComma = data.indexOf(',');
+      int secondComma = data.indexOf(',', firstComma + 1);
 
-    // get advance and turn values
-    advanceTarget = data.substring(0, firstComma).toInt();
-    turnTarget = data.substring(firstComma + 1, secondComma).toInt();
-    servoTarget = data.substring(secondComma + 1).toInt();
+      // get advance and turn values
+      advance_speed = data.substring(0, firstComma).toInt();
+      turn_speed = data.substring(firstComma + 1, secondComma).toInt();
+      command_duration = data.substring(secondComma + 1).toInt();
+      break;
+    }
 
     return true;
   }
@@ -136,51 +173,27 @@ void setup() {
 void loop() {
     
 	if(checkSerialData()){
-    // Reset current values
-    currentTurn = 0;
-    currentAdvance = 0;
-
-    new_data = true;
+    command_start_time = millis();
   };
 
-  // move towards target values
-  if(new_data && (currentTurn != turnTarget || currentAdvance != advanceTarget)){
-    // turn towards target
-    if(currentTurn != turnTarget){
-      if(currentTurn < turnTarget) currentTurn++;
-      else currentTurn--;
-    }
-
-    // advance towards target
-    if(currentAdvance != advanceTarget){
-      if(currentAdvance < advanceTarget) currentAdvance++;
-      else currentAdvance--;
-    }
-
-    moveMotors(currentAdvance, currentTurn);
+  // Move motors
+  if(millis() - command_start_time < command_duration){
+    moveMotors(advance_speed, turn_speed);
   }
-  else if(new_data){
+  else {
     stop();
-    new_data = false;
     Serial.println("STOP");
   }
 
-  //---------------------------------
-  // TEST
-  advance();
-  //---------------------------------
-
-
   // Debug data
   if(DEBUG){
-    Serial.print("Advance: ");
-    Serial.print(currentAdvance);
-    Serial.print(" Turn: ");
-    Serial.print(currentTurn);
-    Serial.print(" Servo: ");
-    Serial.println(servoTarget);
+    Serial.print("Left Motor: ");
+    Serial.print(leftMotorSpeed);
+    Serial.print(" Right Motor: ");
+    Serial.println(rightMotorSpeed);
+    //Serial.print(" Servo: ");
+    //Serial.println(servoTarget);
   }
 
   delay(10);
-
 }
